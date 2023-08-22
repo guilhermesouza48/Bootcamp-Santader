6abceb4f84bdf88d3febd80107009d170f7b9b91
Guilherme Jesus <guilhermesouzacrs@hotmail.com>
2023-08-22 15:40:04 -0300

    feat : :books: Studying variable

---
+++ Aula2-Js/script.js
@@ -0,0 +1,56 @@
+// TIPOS PRIMITIVOS
+var vOuf = false; // Boolean 
+console.log(vOuf);
+console.log(typeof(vOuf));
+
+var numberRandom = 1 
+console.log(numberRandom); // Number
+console.log(typeof(numberRandom));
+
+var stringRandom = "String"
+console.log(stringRandom); // String
+console.log(typeof(stringRandom));
+
+// DECLARAÇÃO
+var variavel = 'gui';
+variavel = 'Guilherme';
+console.log(variavel);
+
+let variavel2 = 'tuguin';
+variavel2 = 'GuizinCrs';
+console.log(variavel2);
+
+const variavel3 = 'Hello';
+variavel3 = 'Hello world';
+console.log(variavel3);
+
+// OPERAÇÕES ARITMETICAS
+
+var soma = 1
+soma = soma + 1
+console.log(soma);
+
+var subtracao = 10
+subtracao = subtracao - 1
+console.log(subtracao);
+
+var multiplicacao = 10
+multiplicacao = multiplicacao * 2
+console.log(multiplicacao);
+
+var divisao = 10
+divisao = divisao / 2
+console.log(divisao);
+
+var resto = 10
+resto = resto % 2
+console.log(resto);
+
+// OPERAÇÕES Lógicas
+
+var verdadeiro = true;
+var falso = false;
+
+console.log(verdadeiro && falso);
+console.log(verdadeiro != falso);
+console.log(falso || verdadeiro);
