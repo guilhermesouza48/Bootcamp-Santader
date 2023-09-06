29c42c1076d46aea525bdf1bd4967f02a257a9ad
Guilherme Jesus <guilhermesouzacrs@hotmail.com>
2023-09-06 19:35:07 -0300

    feat : :books: Function

---
+++ Aula5-Js/script.js
@@ -0,0 +1,13 @@
+function funcao() {
+    console.log('Hello world!');
+}
+
+funcao();
+
+// Função com parâmetros - serve como variável
+
+function carro(cor, marca){
+    console.log(cor, marca);
+}
+
+carro("Amarelo",",Camaro");
